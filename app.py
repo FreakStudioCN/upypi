374ca38d94d744737cb13cce40beff9693d5734d
ben0i0d <ben0i0d@foxmail.com>
2025-12-31 09:58:39 +0000

    update

--- app.py
+++ app.py
@@ -409,4 +409,4 @@ def notfound(e):
     return render_template("404.html"), 404
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(ssl_context=('/tls/cert.crt', '/tls/cert.key'), host="0.0.0.0", port=80)
